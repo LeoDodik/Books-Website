33e75f39853b60b1b13df55ea181200ed79281fd
LeoDodik <119758958+LeoDodik@users.noreply.github.com>
2024-01-16 19:37:06 +0100

    Adding finishing touches

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import express, { response } from 'express';
+import express from 'express';
 import pg from 'pg';
 import bodyParser from 'body-parser';
-import axios from 'axios';
+
 const app = express();
 const port = 3000;
 
@@ -19,25 +19,20 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-
-   
 app.get('/', async (req, res) => {
   try {
-      const response = await axios.get("https://covers.openlibrary.org/b/id/240727-S.jpg");
-      console.log("Full API Response:", response.data); // Log the entire API response
-      const imageUrl = response.data && response.data.url;
-      console.log("Image URL:", imageUrl); // Log the image URL
-
-      res.render("index.ejs", { imageUrl });
+    const result = await db.query("SELECT read_date FROM books WHERE id = 1;");
+    const readingDate = result.rows;
+    const result2 = await db.query("SELECT rating FROM books WHERE id = 1;")
+    const rating = result2.rows
+    res.render("index.ejs", { readingDate, rating});
   } catch (error) {
-      console.error("Error fetching book cover:", error);
-      res.render("error.ejs", { error: "Error fetching book cover" });
+    console.error('Error fetching data from the database:', error);
+    res.status(500).send('Internal Server Error');
   }
+  
 });
 
-
-
- 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
